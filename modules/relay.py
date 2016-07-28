293e2c7acfb8333c44ae8e2d35a603f98024a612
Matthew Beeching <jobe@mdbnet.co.uk>
2016-07-28 15:51:02 -0500

    Changed logging level for relay module

--- modules/relay.py
+++ modules/relay.py
@@ -14,19 +14,19 @@ def bind(type, name, callback):
 	targ = RelayTarget(type, name, callback)
 	if not targ in relaytargets:
 		relaytargets.append(targ)
-		log.log(LOG_INFO, 'Bound relay (type:' + type + ', name:' + name + ')')
+		log.log(LOG_DEBUG, 'Bound relay (type:' + type + ', name:' + name + ')')
 
 def unbind(type, name, callback):
 	global relaytargets
 	targ = RelayTarget(type, name, callback)
 	if targ in relaytargets:
 		relaytargets.remove(targ)
-		log.log(LOG_INFO, 'Unbound relay (type:' + type + ', name:' + name + ')')
+		log.log(LOG_DEBUG, 'Unbound relay (type:' + type + ', name:' + name + ')')
 
 def call(type, name, channel, args):
 	global relaytargets
 	for targ in relaytargets:
 		if targ.type == type and targ.name == name:
 			if targ.callback != None:
-				log.log(LOG_INFO, 'Calling relay (type:' + type + ', name:' + name + ')')
+				log.log(LOG_DEBUG, 'Calling relay (type:' + type + ', name:' + name + ')')
 				targ.callback(channel, args)
