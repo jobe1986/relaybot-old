15e886ee84c3bad9de5428caba58a77d6a2399f3
Matthew Beeching <jobe@mdbnet.co.uk>
2016-07-30 01:20:12 -0500

    Fixed unnecessary upper case naming of relaybindings in modules/relay.py

--- modules/relay.py
+++ modules/relay.py
@@ -5,27 +5,27 @@ from collections import namedtuple
 
 from modules.mylogging import *
 
-RelayBindings = []
+relaybindings = []
 RelayBinding = namedtuple('RelayBinding', ['type', 'name', 'callback'])
 RelayTarget = namedtuple('RelayTarget', ['type', 'name', 'channel', 'prefix'])
 
 def bind(type, name, callback):
-	global RelayBindings
+	global relaybindings
 	targ = RelayBinding(type, name, callback)
-	if not targ in RelayBindings:
-		RelayBindings.append(targ)
+	if not targ in relaybindings:
+		relaybindings.append(targ)
 		log.log(LOG_DEBUG, 'Bound relay (type:' + type + ', name:' + name + ')')
 
 def unbind(type, name, callback):
-	global RelayBindings
+	global relaybindings
 	targ = RelayBinding(type, name, callback)
-	if targ in RelayBindings:
-		RelayBindings.remove(targ)
+	if targ in relaybindings:
+		relaybindings.remove(targ)
 		log.log(LOG_DEBUG, 'Unbound relay (type:' + type + ', name:' + name + ')')
 
 def call(type, name, channel, args):
-	global RelayBindings
-	for targ in RelayBindings:
+	global relaybindings
+	for targ in relaybindings:
 		if targ.type == type and targ.name == name:
 			if targ.callback != None:
 				log.log(LOG_DEBUG, 'Calling relay (type:' + type + ', name:' + name + ')')
