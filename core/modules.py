50287ffa9c6a1d122add022a8fa32737d085dbd9
Matthew Beeching <jobe@mdbnet.co.uk>
2016-08-05 10:04:46 -0500

    Added GPLv3 license and copyright to all source files

--- core/modules.py
+++ core/modules.py
@@ -1,3 +1,22 @@
+# RelayBot - Simple Multi-protocol Relay Bot, core/modules.py
+#
+# Copyright (C) 2016 Matthew Beeching
+#
+# This file is part of RelayBot.
+#
+# RelayBot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# RelayBot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with RelayBot.  If not, see <http://www.gnu.org/licenses/>.
+
 # -*- coding: utf-8 -*-
 
 from collections import namedtuple
