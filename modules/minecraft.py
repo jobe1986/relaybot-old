dda94a1b41901cc47a48714d1b53e4e7c75a70ac
Matthew Beeching <jobe@mdbnet.co.uk>
2017-06-16 14:43:25 -0500

    Another 1.12 related change

--- modules/minecraft.py
+++ modules/minecraft.py
@@ -747,13 +747,13 @@ class playerjoinpartfilter:
 			'net.minecraft.server.MinecraftServer': {
 				'joinpart': '^([^\s]+) (\\(formerly known as .+?\\) )?(?:joined|left) the game$'
 				},
-			'mk': {
+			'pa': {
 				'whitelist': '^com.mojang.authlib.GameProfile.*?id=([-a-f0-9]+?),.*?name=([^,]+?),.+?\\(\\/([0-9\\.]+?):([0-9]+?)\\) lost connection: You are not white-listed on this server!.*$'
 				}
 			}
 	_regc = {
 			'net.minecraft.server.MinecraftServer': {},
-			'mk': {}
+			'pa': {}
 			}
 
 	def filter(self, data):
